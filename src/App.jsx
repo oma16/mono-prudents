8ba3e49c472df63b099955aa02f825e1711e6b8d
oma16 <48384861+oma16@users.noreply.github.com>
2021-12-12 00:06:46 +0100

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from "react-router";
-import Home from "./screens/index";
-import DashboardHome from "./screens/dashboard/index";
-import MentorIndex from './screens/mentor';
-import AdminIndex from './screens/admin';
-import InternIndex from './screens/intern';
+import Home from "./layouts/Index";
+
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/dashboard" element={<DashboardHome />} />
-      <Route path="/mentor" element={<MentorIndex />} />
-      <Route path="/intern" element={<InternIndex />} />
-      <Route path="/admin" element={<AdminIndex />} />
+      
     </Routes>
   );
 }
--- src/screens/dashboard/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function DashboardHome() {
-  return (
-    <h1>
-      Dashboard Index
-    </h1>
-  )
-}
