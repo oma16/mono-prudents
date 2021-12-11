74b552da3b0fa7d972b8b1bd888fab6b5ef0a761
oma16 <48384861+oma16@users.noreply.github.com>
2021-12-12 00:01:52 +0100

    Updates

---
+++ precache-manifest.a3ff664f3d93814a61cb916297b99b04.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185bee96be952d4f61fbe989deec9105",
+    "url": "/mono-prudent/index.html"
+  },
+  {
+    "revision": "95e75d445d0ef405cbd1",
+    "url": "/mono-prudent/static/css/2.fa43bc9e.chunk.css"
+  },
+  {
+    "revision": "be047324ddf884436ba3",
+    "url": "/mono-prudent/static/css/main.196c1e1c.chunk.css"
+  },
+  {
+    "revision": "95e75d445d0ef405cbd1",
+    "url": "/mono-prudent/static/js/2.d733e7a6.chunk.js"
+  },
+  {
+    "revision": "0569f079e111a6d4f0bf8418a634cb07",
+    "url": "/mono-prudent/static/js/2.d733e7a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "934d0eaa7871dd321371",
+    "url": "/mono-prudent/static/js/3.27dc8719.chunk.js"
+  },
+  {
+    "revision": "be047324ddf884436ba3",
+    "url": "/mono-prudent/static/js/main.664385b2.chunk.js"
+  },
+  {
+    "revision": "04f123ede87a78701b58",
+    "url": "/mono-prudent/static/js/runtime-main.88b4d187.js"
+  }
+]);
